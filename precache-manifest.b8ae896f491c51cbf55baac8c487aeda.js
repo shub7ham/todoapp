486a4908847bcf7552fc54055f930cf0194428eb
shub7ham <shubham786bangde@gmail.com>
2019-12-14 13:04:12 +0530

    Updates

---
+++ precache-manifest.b8ae896f491c51cbf55baac8c487aeda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc21d8bec875160c64f0748968ba0b5",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ffd6259843a494505927",
+    "url": "/todoapp/static/css/main.9ff5c6aa.chunk.css"
+  },
+  {
+    "revision": "1a62459e1e88ca644d3d",
+    "url": "/todoapp/static/js/2.7db00eff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoapp/static/js/2.7db00eff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffd6259843a494505927",
+    "url": "/todoapp/static/js/main.1cd8ef0d.chunk.js"
+  },
+  {
+    "revision": "d8ab6d773cc38ec0758d",
+    "url": "/todoapp/static/js/runtime-main.f5ecd5fb.js"
+  }
+]);
